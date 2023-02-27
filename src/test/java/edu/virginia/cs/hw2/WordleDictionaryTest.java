138d7bc0d5dde6d0643d5946e1c8b363c4d17a26
vjz3qz <vjz3qz@virginia.edu>
2023-02-27 18:22:35 -0500

    added legal word, contains for empty dict, and more equivalence test

--- src/test/java/edu/virginia/cs/hw2/WordleDictionaryTest.java
+++ src/test/java/edu/virginia/cs/hw2/WordleDictionaryTest.java
@@ -1,6 +1,7 @@
 package edu.virginia.cs.hw2;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.InputStream;
@@ -17,14 +18,24 @@ public class WordleDictionaryTest {
     }
 
     @Test
+    @DisplayName("checks if one word dict has word and doesn't")
     public void testOneWordDictionary() {
         InputStream inputStream = WordleDictionaryTest.class.getResourceAsStream(ONE_WORD_DICTIONARY_FILENAME);
         testDictionary.addWordsFromInputStream(inputStream);
         assertEquals(1, testDictionary.getDictionarySize());
         assertTrue(testDictionary.containsWord("BALDY"));
+        assertFalse(testDictionary.containsWord("CRATE"));
     }
 
     @Test
+    @DisplayName("checks contain word for empty dictionary")
+    public void testContainsWordForEmptyDictionary()
+    {
+        assertFalse(testDictionary.containsWord("BALDY"));
+    }
+
+    @Test
+    @DisplayName("testing if A123Z is a legal word, should be false")
     public void testLegalWord()
     {
         assertFalse(testDictionary.isLegalWordleWord("A123Z"));
