6e6affca709664ca55db05489c9c381dcaaf2721
ufx2ju <ufx2ju@virginia.edu>
2023-02-28 19:58:44 -0500

    description for test

--- src/test/java/edu/virginia/cs/hw2/GameTest.java
+++ src/test/java/edu/virginia/cs/hw2/GameTest.java
@@ -1,5 +1,6 @@
 package edu.virginia.cs.hw2;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,14 +9,18 @@ class GameTest {
     private GameState testGame;
 
     @Test
+    @DisplayName("Making sure Illegal Answer Doesn't Work")
     public void testConstructorWithIllegalAnswer() {
         assertThrows(IllegalWordException.class,
                 () -> new GameState("QZXYX"));
     }
 
     @Test
+    @DisplayName("Checking If Legal Answer Works")
     public void testConstructorWithLegalAnswer()
     {
         assertNotNull(new GameState("Crate"));
     }
+
+
 }
