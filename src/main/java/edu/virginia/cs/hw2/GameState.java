cae8bfa93037d217ac32c94e97db242a7e200bd3
vjz3qz <vjz3qz@virginia.edu>
2023-02-28 20:24:25 -0500

    made submit guess not return null

--- src/main/java/edu/virginia/cs/hw2/GameState.java
+++ src/main/java/edu/virginia/cs/hw2/GameState.java
@@ -75,7 +75,7 @@ public class GameState {
         guessResult.setGuess(guess);
         guessResult.setAnswer(answer);
 
-        return null;
+        return guessResult.getGuessResult();
         //TODO: Stub - Implement method with TDD - tests must go in GameTest.java
     }
 
